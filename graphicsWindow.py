b933c88c20d164dcb2a1a50baa3d00d8b3547043
Gautam Gupta <gautamgupta9298@gmail.com>
2021-01-14 18:21:10 -0500

    Working algorithm

--- graphicsWindow.py
+++ graphicsWindow.py
@@ -1,3 +1,14 @@
+'''
+Module Name: GraphicsWindow
+Author: Gautam Gupta
+Student #: 250897104
+DOC: 01-22-2021
+
+Purpose: 
+To draw and display an image on a custom sized canvas made from lines given the start and end point. 
+This module implements the Bresenham's algorithm to pick and draw points in between the target points.
+'''
+
 import operator
 from PIL import Image
 from matrix import matrix
@@ -16,20 +27,32 @@ class graphicsWindow:
             self.__image[point[0],point[1]] = color
 
     def drawLine(self,point1,point2,color): 
+        # Convert points from matrix to integer lists
         p1 = [int(point1.get(0,0)), int(point1.get(1,0))]
         p2 = [int(point2.get(0,0)), int(point2.get(1,0))]
 
+        delta = point2.__sub__(point1)
+        delta_x = delta.get(0,0)
+        delta_y = delta.get(1,0)
+
+        # Deciding variables for different slope cases
         steep = negative = False
 
-        delta_x = p2[0] - p1[0]
-        delta_y = p2[1] - p1[1]
-
-        if abs(delta_y) > abs(delta_x): 
+        # Inverse line if slope > 1
+        if(abs(delta_x) < abs(delta_y)):
+            delta_x, delta_y = delta_y, delta_x
             p1[0], p1[1] = p1[1], p1[0]
             p2[0], p2[1] = p2[1], p2[0]
-            delta_x = p2[0] - p1[0]
-            delta_y = p2[1] - p1[1]
             steep = True
+
+        if delta_x < 0:
+            return self.drawLine(point2, point1, color)
+
+        if delta_y < 0:
+            negative = True
+
+        delta_x = abs(delta_x)
+        delta_y = abs(delta_y)
 
         p_i = 2 * delta_y - delta_x
         y = p1[1]
@@ -41,17 +64,20 @@ class graphicsWindow:
             self.drawPoint(draw_point, color)
             p_i, increment = self.bresenhamIncrement(delta_x, delta_y, p_i)
             if increment:
-                y += 1
+                if negative:
+                    y -= 1
+                else:
+                    y += 1
 
+    # Helper function to compute if y_next will increment or stay the same
+    # Accepts and returns p_i value for subsequent computations 
     def bresenhamIncrement(self, delta_x, delta_y, p_i):
         increment = False
         if(p_i < 0):
             p_i += 2*delta_y
-            
         else:
             p_i += 2*delta_y - 2*delta_x
             increment = True
-
         return p_i, increment
 
     def saveImage(self,fileName):
